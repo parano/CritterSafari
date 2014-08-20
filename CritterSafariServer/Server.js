ca3ff0fac8632a46d5c02d668b0f714ff4a2afcd
parano <paranoyang@gmail.com>
2014-08-19 23:20:03 -0700

    add testing mode

--- CritterSafariServer/Server.js
+++ CritterSafariServer/Server.js
@@ -1,19 +1,34 @@
 var express = require('express');
 var GameboardReader = require('./GameboardReader');
 var ReadingQueue = require('./ReadingQueue');
+var readline = require('readline');
 
 var commandQueue = new ReadingQueue();
 //console.log(commandQueue);
 
-var settingBoard = new GameboardReader("/dev/cu.usbmodem1421", 9600, function(data){
-  console.log("data received: " + data);
-  commandQueue.push(data);
-});
+var isTesting = process.argv.length > 2 ? true : false;
 
-var instructionBoard = new GameboardReader("/dev/cu.usbmodem1411", 9600, function(data){
-  console.log("data received: " + data);
-  commandQueue.push(data);
-});
+if(isTesting) {
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.on('line', function(data){
+    console.log("data received: " + data);
+    commandQueue.push(data);
+  });
+} else {
+  var settingBoard = new GameboardReader("/dev/cu.usbmodem1421", 9600, function(data){
+    console.log("data received: " + data);
+    commandQueue.push(data);
+  });
+
+  var instructionBoard = new GameboardReader("/dev/cu.usbmodem1411", 9600, function(data){
+    console.log("data received: " + data);
+    commandQueue.push(data);
+  });
+}
 
 var app = express();
 app.get('/instruction.json', function(req, res){
